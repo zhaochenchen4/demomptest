0f57bd0e822648217e0b7918ce1e0cb8548ce4ba
cczhao <13152928433@163.com>
2022-12-10 23:34:35 +0800

    start

---
+++ src/main/java/com/atguigu/demomptest/DemomptestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.demomptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemomptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/demomptest/DemomptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.demomptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
